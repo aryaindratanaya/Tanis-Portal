a3a1c4d85b857529245f7b375e98cdc96e017f57
Indra Tanaya <aryaindratanaya@gmail.com>
2022-08-12 21:06:54 +0800

    Create `partner` constants

---
+++ src/constants/partner.ts
@@ -0,0 +1,7 @@
+export const partnerTypeSalesPartner = 'Sales Partner'
+export const partnerTypeBoatProvider = 'Boat Provider'
+
+export const partnerType = {
+  SalesPartner: partnerTypeSalesPartner,
+  BoatProvider: partnerTypeBoatProvider,
+}
