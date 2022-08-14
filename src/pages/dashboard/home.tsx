c205bffa620e5297306e48daab716ed1b44301fa
Indra Tanaya <aryaindratanaya@gmail.com>
2022-08-14 09:57:47 +0800

    Create stats card in home

--- src/pages/dashboard/home.tsx
+++ src/pages/dashboard/home.tsx
@@ -1,10 +1,131 @@
 import { NextPage } from 'next'
-import Card from 'antd/lib/card/Card'
+import { Card, Statistic, Row, Col, Tooltip } from 'antd'
+import {
+  ArrowDownOutlined,
+  ArrowUpOutlined,
+  InfoCircleOutlined,
+} from '@ant-design/icons'
 
 const Home: NextPage = () => (
-  <Card>
-    <h1>Hello world</h1>
-  </Card>
+  <Row gutter={[16, 16]}>
+    <Col xs={24} sm={12} md={12} lg={12} xl={6} xxl={6}>
+      <Card
+        title="Tickets"
+        extra={
+          <Tooltip placement="topRight" title="Number of tickets sold today">
+            <InfoCircleOutlined />
+          </Tooltip>
+        }
+      >
+        <Row gutter={[16, 16]}>
+          <Col span={12}>
+            <Statistic
+              title="Foreigner"
+              value={1823}
+              valueStyle={{ color: '#3f8600' }}
+              suffix={<ArrowUpOutlined />}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Local"
+              value={1053}
+              valueStyle={{ color: '#cf1322' }}
+              suffix={<ArrowDownOutlined />}
+            />
+          </Col>
+        </Row>
+      </Card>
+    </Col>
+    <Col xs={24} sm={12} md={12} lg={12} xl={6} xxl={6}>
+      <Card
+        title="Bookings"
+        extra={
+          <Tooltip placement="topRight" title="Number of bookings made today">
+            <InfoCircleOutlined />
+          </Tooltip>
+        }
+      >
+        <Row gutter={[16, 16]}>
+          <Col span={12}>
+            <Statistic
+              title="Single"
+              value={1012}
+              valueStyle={{ color: '#3f8600' }}
+              suffix={<ArrowUpOutlined />}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Group"
+              value={1023}
+              valueStyle={{ color: '#cf1322' }}
+              suffix={<ArrowDownOutlined />}
+            />
+          </Col>
+        </Row>
+      </Card>
+    </Col>
+    <Col xs={24} sm={12} md={12} lg={12} xl={6} xxl={6}>
+      <Card
+        title="Sales Performance"
+        extra={
+          <Tooltip
+            placement="topRight"
+            title="Percentage of tickets sold via each sales channel today"
+          >
+            <InfoCircleOutlined />
+          </Tooltip>
+        }
+      >
+        <Row gutter={[16, 16]}>
+          <Col span={12}>
+            <Statistic
+              title="Organics"
+              value={11.1}
+              valueStyle={{ color: '#3f8600' }}
+              suffix="%"
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Agents"
+              value={89.9}
+              valueStyle={{ color: '#cf1322' }}
+              suffix="%"
+            />
+          </Col>
+        </Row>
+      </Card>
+    </Col>
+    <Col xs={24} sm={12} md={12} lg={12} xl={6} xxl={6}>
+      <Card
+        title="Delivery"
+        extra={
+          <Tooltip
+            placement="topRight"
+            title="Number of passengers transported using in-house boat or partner's boat"
+          >
+            <InfoCircleOutlined />
+          </Tooltip>
+        }
+      >
+        <Row gutter={[16, 16]}>
+          <Col span={12}>
+            <Statistic
+              title="In-House"
+              value={2034}
+              valueStyle={{ color: '#3f8600' }}
+              suffix={<ArrowUpOutlined />}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Transferred" value={0} />
+          </Col>
+        </Row>
+      </Card>
+    </Col>
+  </Row>
 )
 
 export default Home
